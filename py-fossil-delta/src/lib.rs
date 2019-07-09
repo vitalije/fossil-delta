b11e9dcac3104f90a0c07e8178ed187dc8026abd
vitalije <vitalije@kviziracija.net>
2019-07-09 09:09:08 +0200

    py-fossil-delta package exports functions delta and deltainv to Python

--- py-fossil-delta/src/lib.rs
+++ py-fossil-delta/src/lib.rs
@@ -1,10 +1,31 @@
-pub fn add_one(x:u64) -> u64 {
-    x + 1
-}
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+use pyo3::prelude::*;
+
+use fossil_delta::{delta, deltainv};
+
+#[pymodule]
+fn fossil_delta(_py: Python, m:&PyModule) -> PyResult<()> {
+    /// Calculates delta between two given strings a and b
+    ///
+    /// >>> d = fossil_delta.delta(a, b)
+    /// >>> s = fossil_delta.deltainv(b, d)
+    /// >>> assert s == a
+    ///
+    #[pyfn(m, "delta")]
+    fn py_delta(_py: Python, a:&str, b:&str) -> PyResult<String> {
+        let res = delta(a, b);
+        Ok(res)
     }
+    /// Applies delta tp given text and returns changed text
+    ///
+    /// >>> d = fossil_delta.delta(a, b)
+    /// >>> s = fossil_delta.deltainv(b, d)
+    /// >>> assert s == a
+    ///
+    #[pyfn(m, "deltainv")]
+    fn py_deltainv(_py: Python, b:&str, d:&str) -> PyResult<String> {
+        let res = deltainv(b, d);
+        Ok(res)
+    }
+    Ok(())
 }
